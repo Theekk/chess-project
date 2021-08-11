23a22d155c2be7a735b2a6e31de18a8d8ccf18a8
Pedro Ignacio <tk-pedro@hotmail.com>
2021-08-11 20:28:19 -0300

    Add new line

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,5 +8,6 @@ public class Program {
     public static void main(String[] args){
         ChessRules chessRules = new ChessRules();
         UI.printBoard(chessRules.getPieces());
+        System.out.println("AAAA");
     }
 }
