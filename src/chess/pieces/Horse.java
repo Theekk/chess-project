14199bd8c24b4482334c80e11e399240eb53c8c0
Pedro Ignacio <tk-pedro@hotmail.com>
2021-08-17 14:12:16 -0300

    Horse moves

---
+++ src/chess/pieces/Horse.java
@@ -0,0 +1,94 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Horse extends ChessPiece {
+
+    public Horse(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString(){
+        return"H";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mov = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0,0);
+
+        //UpRight
+        p.setValues(position.getRow() - 2, position.getColumn() + 1);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+        // UpLeft
+        p.setValues(position.getRow() - 2, position.getColumn() - 1);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+
+        //RightUp
+        p.setValues(position.getRow() -1, position.getColumn() + 2);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+        //RightDown
+        p.setValues(position.getRow() + 1, position.getColumn() + 2);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+
+        //DownRight
+        p.setValues(position.getRow() + 2, position.getColumn() + 1);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+        //DownLeft
+        p.setValues(position.getRow() + 2, position.getColumn() - 1);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+
+        //LeftUp
+        p.setValues(position.getRow() - 1, position.getColumn() - 2);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+        //LeftDown
+        p.setValues(position.getRow() + 1, position.getColumn() - 2);
+        if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mov[p.getRow()][p.getColumn()] = true;
+        }
+
+        return mov;
+    }
+}
